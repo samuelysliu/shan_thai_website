d84c9edf32ecafb9da0bc2da1271a2d20f5e51e4
samuelysliu <samuelysliu@gmail.com>
2024-12-15 10:18:48 +0800

    add flex apiBaseUrl

--- Views/src/app/config.js
+++ Views/src/app/config.js
@@ -1,6 +1,11 @@
+const api_url = {
+  production: "",
+  uat: "https://miserable-alina-samuelysliu-96038ede.koyeb.app",
+  local: "http://localhost:8000"
+}
+
 const config = {
-    apiBaseUrl: "http://localhost:8000",
+  apiBaseUrl: api_url[process.env.APP_ENV],
   };
   
   export default config;
-  
