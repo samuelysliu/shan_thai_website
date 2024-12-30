be913b7ad9d1fcc11d18bc4834c29c117df94143
samuelysliu <samuelysliu@gmail.com>
2024-12-30 13:16:27 +0800

    fixed db connect

--- modules/dbConnect.py
+++ modules/dbConnect.py
@@ -8,9 +8,17 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, pool_recycle=1800)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=10,
+    max_overflow=2,
+    pool_recycle=300,
+    pool_pre_ping=True,
+    pool_use_lifo=True,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
