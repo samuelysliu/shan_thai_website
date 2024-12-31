8615fffc84d3a9205d55ccbcabf1346b416266ab
samuelysliu <samuelysliu@gmail.com>
2024-12-31 11:46:21 +0800

    fixed dockerfile

--- views/next.config.mjs
+++ views/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+
+    // 將環境變數暴露給應用程序
+    env: {
+        NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV || "production", // 默認值為 "local"
+    },
 };
 
 export default nextConfig;
