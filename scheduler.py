486e9fbb54ba51a3f9b1d74d901467b3b0e9c62b
samuelysliu <samuelysliu@gmail.com>
2025-02-27 10:35:26 +0800

    fixed scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,6 @@
 from controls.cash_flow import check_order
 from controls.logistic import check_logistic_status
+import asyncio
 
 def check_cashflow_order_scheduler():
     print("check_cashflow_order start")
@@ -11,5 +12,5 @@ async def check_logisitic_order_scheduler():
     await check_logistic_status()
     print("check_logistic_status end")
     
+asyncio.run(check_logisitic_order_scheduler())
 check_cashflow_order_scheduler()
-check_logisitic_order_scheduler()
